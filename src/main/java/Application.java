631c07bbe72c9a47ecb90e905bf8e26c7a1a21aa
Alexander Berg <alexander.berg.p92@sonymusic.com>
2019-12-06 10:30:33 +0100

    more generic jpg usage

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -7,36 +7,61 @@ import com.itextpdf.text.pdf.PdfWriter;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Application {
-    public static void main(String[] args) throws IOException, DocumentException {
-        File rootImages = new File("./src/main/resources/images");
-        File rootPdf = new File("./src/main/resources/pdf");
-        String outputFile = "output.pdf";
+	private static final String JPG_DIR_PATH = "c:\\jpgs";
 
-        List<String> files = new ArrayList<String>();
-        files.add("test1.jpg");
-        files.add("test2.jpg");
-        files.add("test3.jpg");
+	public static void main(String[] args) {
+		renameSequence();
+		try (Stream<Path> walk = Files.walk(Paths.get(JPG_DIR_PATH))) {
+			Document document = new Document();
+			PdfWriter.getInstance(document, new FileOutputStream(new File(JPG_DIR_PATH, "book.pdf")));
+			document.open();
+			walk.map(x -> x.toString()).filter(f -> f.toLowerCase().endsWith(".jpg") || f.toLowerCase().endsWith(".jpeg")).forEach(filePath -> {
+				document.newPage();
+				try {
+					System.out.println(filePath);
+					Image image = Image.getInstance(filePath);
+					image.setAbsolutePosition(0, 0);
+					image.setBorderWidth(0);
+					image.scaleToFit(PageSize.A4.getWidth(), PageSize.A4.getHeight());
 
-        Document document = new Document();
-        PdfWriter.getInstance(document, new FileOutputStream(new File(rootPdf, outputFile)));
-        document.open();
-        for (String f : files) {
-            document.newPage();
-            Image image = Image.getInstance(new File(rootImages, f).getAbsolutePath());
-            image.setAbsolutePosition(0, 0);
-            image.setBorderWidth(0);
-            image.scaleToFit(PageSize.A4.getWidth(), PageSize.A4.getHeight());
+					float x = (PageSize.A4.getWidth() - image.getScaledWidth()) / 2;
+					float y = (PageSize.A4.getHeight() - image.getScaledHeight()) / 2;
+					image.setAbsolutePosition(x, y);
+					document.add(image);
+				} catch (IOException | DocumentException e) {
+					e.printStackTrace();
+				}
+			});
+			document.close();
+		} catch (IOException | DocumentException e) {
+			e.printStackTrace();
+		}
+	}
 
-//            Centreren in x en y richting van de plaatjes
-            float x = (PageSize.A4.getWidth() - image.getScaledWidth()) / 2;
-            float y = (PageSize.A4.getHeight() - image.getScaledHeight()) / 2;
-            image.setAbsolutePosition(x, y);
-            document.add(image);
-        }
-        document.close();
-    }
+	private static void renameSequence() {
+		try (Stream<Path> walk = Files.walk(Paths.get(JPG_DIR_PATH))) {
+			walk.map(x -> x.toString()).filter(f -> f.toLowerCase().endsWith(".jpg") || f.toLowerCase().endsWith(".jpeg")).forEach(filePath -> {
+				String sequence = filePath.substring(filePath.lastIndexOf("-") + 1, filePath.lastIndexOf("."));
+				System.out.println(sequence);
+				if (sequence.length() == 2) {
+					File file = new File(filePath);
+					file.renameTo(new File(file.toString().replace(sequence, "00" + sequence)));
+				} else if (sequence.length() == 3) {
+					File file = new File(filePath);
+					file.renameTo(new File(file.toString().replace(sequence, "0" + sequence)));
+				}
+			});
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
